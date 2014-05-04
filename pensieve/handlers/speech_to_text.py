609fd33b3c219528a9dc04a0de2da0bd90cb5654
rickyhopper <Ricky@Richards-MacBook-Pro-2.local>
2014-05-04 16:00:05 -0400

    Changed a few things so that it works. And it does!

--- pensieve/handlers/speech_to_text.py
+++ pensieve/handlers/speech_to_text.py
@@ -15,6 +15,7 @@ from ..helpers import ok, invalidValue, checkField, MessageError
 
 class SpeechToText(BaseHandler):
     audio = None
+    directoryPath = None
 
     def handle(self, header, data):
         #ensure the request contains the necessary data
@@ -24,26 +25,38 @@ class SpeechToText(BaseHandler):
         except MessageError as e:
             return e.json
 
-        #create directory path for file
-        directoryPath, thisName = os.path.split(os.path.abspath(__file__))
-        directoryPath += os.sep
+        #create directory path for file if there is none
+        if self.directoryPath == None:
+            path, thisName = os.path.split(os.path.abspath(__file__))
+            path += os.sep + datetime.today().isoformat().replace(':','-')
+            os.system("mkdir " + path)
+            path += os.sep
+            self.directoryPath = path
         #create a new file with a unique filepath
         fName = "sound-" + datetime.today().isoformat().replace(':','-');
-        m4aFile = open(directoryPath + fName + ".m4a", "wb")
+        m4aFile = open(self.directoryPath + fName + ".m4a", "wb")
         m4aFile.write(data)
         m4aFile.close()
         """
         NOTE: I may have to synchronize this block of the program if we end up doing async processing...
         Otherwise, it will be bad times
+        NOTE 2: Concatenating flac files isn't working, so I'm concatenating everything in m4a format
+        and then converting the final result to flac for processing.
         """
-        #run ffmpeg to convert to flac
-        os.system("ffmpeg -i " + directoryPath + fName + ".m4a -acodec flac -ab 16k " + directoryPath + fName + ".flac")
+        os.system("rm " + self.directoryPath + "list.txt")
+
         if self.audio == None:
-            os.system("cp " + directoryPath + fName + ".flac " + directoryPath + "workingAudio.flac")
+            os.system("cp " + self.directoryPath + fName + ".m4a " + self.directoryPath + "workingAudio.m4a")
         else:
-            os.system("sox " + directoryPath + "workingAudio.flac " + directoryPath + fName + ".flac " + directoryPath + "workingAudio.flac")
+            os.system("mv " + self.directoryPath + "workingAudio.m4a " + self.directoryPath + "workingAudio_temp.m4a")
+            os.system("echo file '" + self.directoryPath + "workingAudio_temp.m4a' >> " + self.directoryPath + "list.txt")
+            os.system("echo file '" + self.directoryPath + fName + ".m4a' >> " + self.directoryPath + "list.txt")
+            os.system("ffmpeg -f concat -i " + self.directoryPath + "list.txt -c copy " + self.directoryPath + "workingAudio.m4a")
+            os.system("rm " + self.directoryPath + "workingAudio_temp.m4a")
+        #run ffmpeg to convert to flac
+        os.system("ffmpeg -i " + self.directoryPath + "workingAudio.m4a -acodec flac -ab 16k " + self.directoryPath + "workingAudio.flac")
         #get bytes from working stream
-        flacFile = open("workingAudio.flac", "rb")
+        flacFile = open(self.directoryPath + "workingAudio.flac", "rb")
         flacData = flacFile.read()
         flacFile.close()
         """
@@ -69,19 +82,20 @@ class SpeechToText(BaseHandler):
         print response
 
         try:
-            jsonData = json.loads(str(response))
+            jsonData = json.loads(str(response).split('\n')[1])
+            print jsonData
             if (len(jsonData["result"]) > 0):
                 #words found
                 #reduce working audio stream to only the most recent file
-                os.system("rm " + directoryPath + "workingAudio.flac")
-                os.system("mv " + directoryPath + fName + ".flac " + directoryPath + "workingAudio.flac")
+                os.system("rm " + self.directoryPath + "workingAudio.m4a")
+                os.system("mv " + self.directoryPath + fName + ".m4a " + self.directoryPath + "workingAudio.m4a")
             else:
-                os.system("rm " + directoryPath + fName + ".flac")
+                os.system("rm " + self.directoryPath + fName + ".m4a")
             ret = json.dumps(jsonData)
+            os.system("rm " + self.directoryPath + "workingAudio.flac")
+            return ret
         except Exception as e:
             print e
+            os.system("rm " + self.directoryPath + fName + ".m4a")
+            os.system("rm " + self.directoryPath + "workingAudio.flac")
             return invalidValue("life")
-
-        os.system("rm " + directoryPath + fName + ".m4a")
-
-        return ret
